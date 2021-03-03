7d7f108ff558396c423c0d84ffc3e33392bef35c
Adam <adamskoullos@gmail.com>
2021-03-03 17:07:00 +0000

    home.vue - using watch()

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="home">
-    <h1>Home Page</h1>
+    <input type="text" v-model="search">
+    <h5>Search term: {{ search }}</h5>
+    <div v-for="car in matchingCars" :key="car">{{ car }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
+import { computed, ref } from '@vue/reactivity'
+import { watch } from '@vue/runtime-core';
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup(){
+    
+    const search = ref('');
+    const cars = ref(['Supra', 'GT86', 'E36', 'RX7', 'E30', '200SX'])
+    
+    const stopWatching = watch(search, () => {
+      console.log('Watch fired')
+    })
+    
+    const matchingCars = computed(() => {
+      return cars.value.filter((car) => car.toLowerCase().includes(search.value.toLowerCase()))
+    })
+
+    const handleClick = () => {
+      stopWatching()
+    }
+
+    return { search, cars, matchingCars, handleClick }
+  }
 }
 </script>
