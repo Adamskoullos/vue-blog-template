6029775e00e40facfc1bdf22d49cbdbdc57070de
Adam <adamskoullos@gmail.com>
2021-03-04 12:31:58 +0000

    Async fetch - pattern

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <h1>Blog</h1>
-    <PostList :posts="posts" />
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading....</div>
   </div>
 </template>
 
@@ -14,12 +18,26 @@ export default {
   components: { PostList },
   setup(){
     
-    const posts = ref([
-      {title: 'Blog Post One', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est.', id: 1},
-      {title: 'Blog Post Two', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est.', id: 2}
-    ])
+    const posts = ref([]);
+    const error = ref(null);
+    const uri = 'http://localhost:3000/posts';
 
-    return { posts }
+    const load = async () => {
+      try{
+        let data = await fetch(uri)
+        if(!data.ok){
+          throw Error('No data available');
+        }
+        posts.value = await data.json();
+      }
+      catch(err){
+        error.value = err.message; 
+        console.log(error.value);
+      }
+    }
+    load()
+
+    return { posts, error }
   }
 }
 </script>
